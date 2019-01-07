5d9ef34b6066de0a55fcce84143c9ee0bd4f39cf
Yuri Korman <korman.yuri@gmail.com>
2019-01-08 00:37:17 +0200

    fixed bug

--- src/Command/LitresUpdateDleCommand.php
+++ src/Command/LitresUpdateDleCommand.php
@@ -222,9 +222,9 @@ class LitresUpdateDleCommand extends Command
 
         if (isset($data['title-info']['annotation'])) {
 
-            print_r($data['title-info']['annotation']);
+            //print_r($data['title-info']['annotation']);
 
-            if (is_array($data['title-info']['annotation']['p'])) {
+            if (isset($data['title-info']['annotation']['p']) && is_array($data['title-info']['annotation']['p'])) {
                 return $data['title-info']['annotation']['p'][0];
             } else {
                 return $data['title-info']['annotation']['p'];
